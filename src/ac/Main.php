7bbedbd62814b26bbf4ee3d6d575e4a30f088020
tigerza117 <noppawanza1171@gmail.com>
2017-04-04 11:57:47 +0700

    อัพเดทกันอีกพวกหัวควยกระโดสูง
    อัพเดทกันอีกพวกหัวควยกระโดสูง

--- src/ac/Main.php
+++ src/ac/Main.php
@@ -5,31 +5,40 @@
     use pocketmine\event\Listener;
     use pocketmine\plugin\PluginBase;
     use pocketmine\network\protocol\AdventureSettingsPacket;
-    use pocketmine\network\protocol\SetPlayerGameTypePacket;
+    use pocketmine\network\protocol\PlayerActionPacket;
     use pocketmine\event\server\DataPacketReceiveEvent;
+    use pocketmine\event\player\PlayerJoinEvent;
+    use pocketmine\event\player\PlayerMoveEvent;
+    use pocketmine\entity\Effect;
+    use pocketmine\utils\TextFormat;
 
     class Main extends PluginBase implements Listener {
+
+        public $players = [];
 
         public function onEnable() {
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
         }
 
+        public function onPlayerJoin(PlayerJoinEvent $event){
+            $this->players[$event->getPlayer()->getName()] = 0;
+    	}
+
         public function onRecieve(DataPacketReceiveEvent $event) {
             $player = $event->getPlayer();
             $packet = $event->getPacket();
             if ($packet instanceof AdventureSettingsPacket) {
-                $event->setCancelled();
                 switch ($packet->flags) {
                     case 614:
                         if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump("ไอสัส ".$player->getName()." Hack ลอยขึ้น");
-                            $player->kick("ไอสัส HACK");
+                            var_dump("ไอสัส ".$player->getName()." Hack Fly");
+                            $player->kick("ไอสัส HACK Fly");
                         }
                         break;
                     case 102:
                         if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight()){
-                            var_dump("ไอสัส ".$player->getName()." Hack ลอยลง");
-                            $player->kick("ไอสัส HACK");
+                            var_dump("ไอสัส ".$player->getName()." Hack Fly");
+                            $player->kick(TextFormat::RED."ไอสัส HACK Fly");
                         }
                         break;
                     default:
@@ -38,5 +47,31 @@
                 }
                 var_dump($packet->flags);
             }
+            if ($packet instanceof PlayerActionPacket) {
+                switch ($packet->action) {
+                    case 8:
+                        var_dump("โดด");
+                        break;
+                    default:
+                        # code...
+                        break;
+                }
+                var_dump($packet->action);
+            }
         }
+
+        public function PlayerMove(PlayerMoveEvent $event){
+            $player = $event->getPlayer();
+            if(!$player->isCreative() and !$player->isSpectator() and !$player->isOp() and !$player->getAllowFlight() and !$player->hasEffect(Effect::JUMP)){
+                if(round($event->getTo()->getY() - $event->getFrom()->getY(),3) >= 0.375 or round($event->getTo()->getY() - $event->getFrom()->getY(),3) === -0.375) {
+                    $this->players[$name] ++;
+                }else{
+                    $this->players[$name] = 0;
+                }
+                if($this->players[$name] >= 3){
+	                var_dump("ไอสัส ".$player->getName()." Hack โดดสูง หรือ Fly");
+                    $player->kick(TextFormat::RED."ไอสัส Hack โดดสูง หรือ Fly");
+	            }
+            }
+	    }
     }
